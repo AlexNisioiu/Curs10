17f675d6a5fc39461a28c4b394adf5da12a9ed00
Alex Nisioiu <alexnisioiu1@gmail.com>
2024-05-16 17:53:43 +0300

    curs10

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,15 @@ public class Main {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
+
+            Incepator();
         }
     }
 
-    public static void Incepator();
+    public static void Incepator(){
+        int x = 5;
+        int y = 10;
+        System.out.println(x + y);
+    }
 
 }
